07e59b02cae1ba58b405f0d13c2a052b3c98eb6a
Ranjeet1508 <danavrj1998@gmail.com>
2023-04-22 20:14:39 +0530

    landing page

--- Landing_Page/landingpage.js
+++ Landing_Page/landingpage.js
@@ -36,3 +36,74 @@ function slidePic(){
     x[slideIndex-1].style.display = "block";
     setTimeout(slidePic,3000);
 }
+
+    
+    
+    function addToKart(){
+        var cartArr = JSON.parse(localStorage.getItem("cart-list")) || [];
+        var obj = {
+            img: document.getElementById("img1").src,
+            para1: document.getElementById("para1").textContent,
+            para2: document.getElementById("para11").textContent
+        };
+        console.log(obj);
+        cartArr.push(obj);
+        console.log(cartArr);
+        localStorage.setItem("cart-list",JSON.stringify(cartArr));
+    }
+
+
+    function addToKart1(){
+        var cartArr = JSON.parse(localStorage.getItem("cart-list")) || [];
+        var obj = {
+            img: document.getElementById("img2").src,
+            para1: document.getElementById("para2").textContent,
+            para2: document.getElementById("para21").textContent
+        };
+        console.log(obj);
+        cartArr.push(obj);
+        console.log(cartArr);
+        localStorage.setItem("cart-list",JSON.stringify(cartArr));
+    }
+
+
+    function addToKart2(){
+        var cartArr = JSON.parse(localStorage.getItem("cart-list")) || [];
+        var obj = {
+            img: document.getElementById("img3").src,
+            para1: document.getElementById("para3").textContent,
+            para2: document.getElementById("para31").textContent
+        };
+        console.log(obj);
+        cartArr.push(obj);
+        console.log(cartArr);
+        localStorage.setItem("cart-list",JSON.stringify(cartArr));
+    }
+
+
+    function addToKart3(){
+        var cartArr = JSON.parse(localStorage.getItem("cart-list")) || [];
+        var obj = {
+            img: document.getElementById("img4").src,
+            para1: document.getElementById("para4").textContent,
+            para2: document.getElementById("para41").textContent
+        };
+        console.log(obj);
+        cartArr.push(obj);
+        console.log(cartArr);
+        localStorage.setItem("cart-list",JSON.stringify(cartArr));
+    }
+
+
+    function addToKart4(){
+        var cartArr = JSON.parse(localStorage.getItem("cart-list")) || [];
+        var obj = {
+            img: document.getElementById("img5").src,
+            para1: document.getElementById("para5").textContent,
+            para2: document.getElementById("para51").textContent
+        };
+        console.log(obj);
+        cartArr.push(obj);
+        console.log(cartArr);
+        localStorage.setItem("cart-list",JSON.stringify(cartArr));
+    }
